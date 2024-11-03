978cc891d5bc947463544b1144e4c778e5046bf2
vssukharev <sugharevmail@gmail.com>
2024-11-03 23:23:52 +0200

    Deleted everything in respect of usage of libclang

--- shell.nix
+++ shell.nix
@@ -1,6 +1,12 @@
 { pkgs ? import <nixpkgs> {} }: with pkgs;
 mkShell {
-  buildInputs = [ c3c ];
+  buildInputs = [ 
+    c3c
+    libclang.lib
+  ];
+
   shellHook = ''zsh'';
+
+  LIBRARY_PATH = builtins.concatStringsSep ":" [ "${libclang.lib}/lib" ];
 }
 
