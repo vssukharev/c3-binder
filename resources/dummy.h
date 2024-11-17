62f3e4852b6abbf077f09b9eaa487055f2fbbe5d
vssukharev <sugharevmail@gmail.com>
2024-11-17 15:21:13 +0200

    Added macro lexing

--- resources/dummy.h
+++ resources/dummy.h
@@ -1,7 +1,7 @@
 // -> module dummy;
 
 
-#define MACRO_VAL 1 // -> const MACRO_VAL = 1;
+#define MACRO_VAL 1 /* -> const MACRO_VAL = 1; */
 #define MACRO_FUN(base) base##t // unchanged
 
 int f(float a, double);
