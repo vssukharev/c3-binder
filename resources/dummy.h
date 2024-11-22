9cf51797a577b96d8a62c9d772b353c2c002f403
vssukharev <sugharevmail@gmail.com>
2024-11-22 11:10:06 +0200

    binder::trans::clexer - implemented spaces deletion before macro body

--- resources/dummy.h
+++ resources/dummy.h
@@ -1,6 +1,6 @@
 // -> module dummy;
 
-#define MACRO_VAL 1 /* -> const MACRO_VAL = 1; */
+#define MACRO_VAL   1 /* -> const MACRO_VAL = 1; */
 #define MACRO_FUN(base) base##t // unchanged
 
 int f(float a, double);
